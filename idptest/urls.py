1349aa6c297664863cc992b9af110bbe7f890221
Thomas Capricelli <capricelli@sylphide-consulting.com>
2018-02-26 17:53:13 +0100

    patterns is obsolete with recent Django

--- idptest/urls.py
+++ idptest/urls.py
@@ -8,11 +8,11 @@ admin.autodiscover()
 
 urlpatterns = [
     # Example:
-    # (r'^idptest/', include('idptest.foo.urls')),
+    # url(r'^idptest/', include('idptest.foo.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', admin.site.urls),
