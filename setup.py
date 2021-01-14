2c2273be5418ae756a5987d632ce6dec6e2fabe3
Mike Rosengrant <mrosengrant@zipari.com>
2021-01-14 11:16:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     long_description='\n\n'.join([description, changelog]),
     install_requires=[
         'Django>=1.9.*',
-        'M2Crypto==0.35.2',
+        'M2Crypto==0.37.1',
         'beautifulsoup4>=4.7.1',
         'structlog'
     ],
